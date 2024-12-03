abcc564c5cfc3550fcf80559fc071dfdfda1357c
vista-man <524715@vistacollege.nl>
2024-12-03 10:01:05 +0100

    y

--- assets/php/fetch_comments.php
+++ assets/php/fetch_comments.php
@@ -3,7 +3,7 @@
 $conn = mysqli_connect('localhost:3306', 'Skillbuddy', '#45Vxt6b7', 'boydfranken_nl_');
 
 // Fetch comments
-$sql = "SELECT text FROM comments";
+$sql = "SELECT text FROM Comments";
 $result = mysqli_query($conn, $sql);
 
 $comments = [];
--- assets/php/submit_comment.php
+++ assets/php/submit_comment.php
@@ -6,7 +6,7 @@ $conn = mysqli_connect('localhost:3306', 'Skillbuddy', '#45Vxt6b7', 'boydfranken
 $comment = $_POST['comment'];
 // ... (validation logic)
 
-$sql = "INSERT INTO comments (text) VALUES (?)";
+$sql = "INSERT INTO Comments (text) VALUES (?)";
 $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, 's', $comment);
 mysqli_stmt_execute($stmt);
